d7ce30a3187b04ced01f1e02b2f228b50c9f885e
Pablo Vargas <85257320+Zettaiki@users.noreply.github.com>
2022-07-27 14:43:55 +0200

    Create ProyectPanel.java

---
+++ src/gui/ProyectPanel.java
@@ -0,0 +1,63 @@
+package gui;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class ProyectPanel extends JPanel {
+    private static final long serialVersionUID = 8475751505006519027L;
+    public final GridBagConstraints c;
+    
+    public ProyectPanel() {
+        this.c = new GridBagConstraints();
+        this.setLayout(new GridBagLayout());
+        
+        // First column
+        
+        c.gridx = 0;
+        c.gridy = 0;
+        var a0 = new JLabel("Opzioni:");
+        this.add(a0, c);
+        
+        c.gridx = 0;
+        c.gridy = 1;
+        var a1 = new JButton("Aggiungere progetto");
+        a1.setLayout(null);
+        a1.addActionListener(e -> {
+        	// TODO
+        });
+        this.add(a1, c);
+        
+        c.gridx = 0;
+        c.gridy = 2;
+        var a2 = new JButton("Modificare progetto");
+        a2.setLayout(null);
+        a2.addActionListener(e -> {
+        	// TODO
+        });
+        this.add(a2, c);
+        
+        c.gridx = 0;
+        c.gridy = 3;
+        var a3 = new JButton("Lista progetti attivi");
+        a3.setLayout(null);
+        a3.addActionListener(e -> {
+        	// TODO
+        });
+        this.add(a3, c);
+        
+        // Second column
+        
+        c.gridx = 1;
+        c.gridy = 0;
+        var b0 = new JLabel("Progetti registrati:");
+        this.add(b0, c);
+        
+        c.gridx = 1;
+        c.gridy = 1;
+        c.gridheight = 5;
+        var textArea = new JTextArea(10, 40);
+        var b1 = new JScrollPane(textArea);
+        textArea.setEditable(false);
+        this.add(b1, c);
+    }
+}

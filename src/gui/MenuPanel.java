b0dbd938984e0e93bc9498ad81631f44b80049e0
Pablo Vargas <85257320+Zettaiki@users.noreply.github.com>
2022-07-28 01:32:08 +0200

    Update MenuPanel.java

--- src/gui/MenuPanel.java
+++ src/gui/MenuPanel.java
@@ -8,7 +8,7 @@ import java.awt.*;
 
 public class MenuPanel extends AbstractGridBagLayoutJPanel {
     private static final long serialVersionUID = 8475751505006519027L;
-
+    
     public MenuPanel() {
         super("EAMS - Main menu", new Dimension(GUI.getMinScreenSize()*12/16, GUI.getMinScreenSize()*9/16));
         
@@ -27,46 +27,48 @@ public class MenuPanel extends AbstractGridBagLayoutJPanel {
         
         // Menu principale
         
-        JPanel menuPanel = new JPanel(new FlowLayout());
-        
         c.gridx = 0;
-        c.gridy = 0;
+        c.gridy = 2;
         var a0 = new JButton("Area progetti e donazioni");
         a0.addActionListener(e -> {
         	JFrame parent = JComponentLoader.getParentFrame(this);
-            JComponentLoader.load(parent, new ProyectMenuPanel());
+            JComponentLoader.load(parent, new ProjectMenuPanel());
         });
-        menuPanel.add(a0, c);
-        
-        c.gridx = 1;
-        c.gridy = 0;
-        var a1 = new JButton("Prodotti e vendite");
-        a1.addActionListener(e -> {
-        	// TODO
-        });
-        menuPanel.add(a1, c);
-        
-        c.gridx = 2;
-        c.gridy = 0;
-        var a2 = new JButton("Gestione rifiuti");
-        a2.addActionListener(e -> {
-        	// TODO
-        });
-        menuPanel.add(a2, c);
-        
-        c.gridx = 0;
-        c.gridy = 2;
-        this.add(menuPanel, c);
-        
-        // Logout e bottoni finali
+        this.add(a0, c);
         
         c.gridx = 0;
         c.gridy = 3;
+        var a1 = new JButton("Volontariato");
+        a1.addActionListener(e -> {
+        	// TODO
+        });
+        this.add(a1, c);
+        
+        c.gridx = 0;
+        c.gridy = 4;
+        var a2 = new JButton("Eventi e servizi");
+        a2.addActionListener(e -> {
+        	// TODO
+        });
+        this.add(a2, c);
+        
+        c.gridx = 0;
+        c.gridy = 5;
+        var a3 = new JButton("Newsletter");
+        a3.addActionListener(e -> {
+        	// TODO
+        });
+        this.add(a3, c);
+
+        // Logout e bottoni finali
+        
+        c.gridx = 0;
+        c.gridy = 6;
         var b0 = new JButton("Logout");
         b0.addActionListener(e -> {
         	JFrame parentFrame = (JFrame) SwingUtilities.getWindowAncestor(this);
              JComponentLoader.load(parentFrame, new LoginPanel());
         });
-        menuPanel.add(b0, c);
+        this.add(b0, c);
     }
 }

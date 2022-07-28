a97c755bf201f8222ff8e081b1599b08eb9ccf0b
Pablo Vargas <85257320+Zettaiki@users.noreply.github.com>
2022-07-28 12:15:40 +0200

    Create InsertProyectPanel.java

---
+++ src/gui/proyect_menu/InsertProyectPanel.java
@@ -0,0 +1,113 @@
+package gui.proyect_menu;
+
+import javax.swing.*;
+import java.awt.*;
+import gui.AbstractGridBagLayoutJPanel;
+import gui.GUI;
+import utils.JComponentLoader;
+
+public class InsertProyectPanel extends AbstractGridBagLayoutJPanel {
+    private static final long serialVersionUID = 8475751505006519027L;
+
+    public InsertProyectPanel() {
+        super("EAMS - Volunteering", new Dimension(GUI.getMinScreenSize()*14/16, GUI.getMinScreenSize()*12/16));
+        
+        // Titolo schermata
+        
+        c.gridx = 0;
+        c.gridy = 0;
+        c.gridwidth = 2;
+        this.add(new JLabel("Ecological Association Management System - Inserimento donazione"), c);
+        
+        // First column
+        
+        var a1 = new JPanel();
+        {
+        	c.gridx = 0;
+            c.gridy = 0;
+            c.gridwidth = 1;
+            var dayLabel = new JLabel("Giorno: ");
+            a1.add(dayLabel, c);
+        	
+        	c.gridx = 1;
+            c.gridy = 0;
+            var dayTextArea = new JTextArea(1,2);
+            dayTextArea.setBorder(BorderFactory.createLineBorder(Color.black));
+            a1.add(dayTextArea, c);
+            
+            c.gridx = 0;
+            c.gridy = 1;
+            c.gridwidth = 1;
+            var monthLabel = new JLabel("Mese: ");
+            a1.add(monthLabel, c);
+            
+            c.gridx = 1;
+            c.gridy = 1;
+            var monthTextArea = new JTextArea(1,2);
+            monthTextArea.setBorder(BorderFactory.createLineBorder(Color.black));
+            a1.add(monthTextArea, c);
+            
+            c.gridx = 0;
+            c.gridy = 2;
+            c.gridwidth = 1;
+            var yearLabel = new JLabel("Anno: ");
+            a1.add(yearLabel, c);
+            
+            c.gridx = 1;
+            c.gridy = 2;
+            var yearTextArea = new JTextArea(1,4);
+            yearTextArea.setBorder(BorderFactory.createLineBorder(Color.black));
+            a1.add(yearTextArea, c);
+        }
+        c.gridx = 0;
+        c.gridy = 1;
+        a1.setBorder(BorderFactory.createTitledBorder("Data inizio:"));
+        this.add(a1, c);
+        
+        var a2 = new JPanel();
+        {
+        	c.gridx = 0;
+            c.gridy = 2;
+            var monthLabel = new JLabel("Durata mesi: ");
+            a2.add(monthLabel, c);
+            
+            c.gridx = 1;
+            c.gridy = 2;
+            var monthTextArea = new JTextArea(1, 2);
+            monthTextArea.setBorder(BorderFactory.createLineBorder(Color.black));
+            a2.add(monthTextArea, c);
+        }
+        c.gridx = 0;
+        c.gridy = 2;
+        a2.setBorder(BorderFactory.createTitledBorder(""));
+        this.add(a2, c);
+        
+        c.gridx = 1;
+        c.gridy = 1;
+        c.gridheight = 2;
+        var a3 = new JTextArea(5, 25);
+        a3.setBorder(BorderFactory.createTitledBorder("Obiettivo progetto:"));
+        this.add(a3, c);
+        
+        // End panel
+        
+        c.gridx = 0;
+        c.gridy = 3;
+        c.gridwidth = 2;
+        c.gridheight = 1;
+        var b0 = new JButton("Crea progetto");
+        b0.addActionListener(e -> {
+	        // TODO
+        });
+        this.add(b0, c);
+        
+        c.gridx = 0;
+        c.gridy = 4;
+        var b1 = new JButton("Ritorna");
+        b1.addActionListener(e -> {
+	        JFrame parentFrame = (JFrame) SwingUtilities.getWindowAncestor(this);
+	        JComponentLoader.load(parentFrame, new ProjectMenuPanel());
+        });
+        this.add(b1, c);
+    }
+}

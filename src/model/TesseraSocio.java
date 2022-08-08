b0c5ba313ca878137f86ed2c084aac2c54f25102
Sara Romeo <sara.romeo3@studio.unibo.it>
2022-08-08 19:47:22 +0200

    Implement interface methods

---
+++ src/model/TesseraSocio.java
@@ -0,0 +1,52 @@
+package model;
+
+import java.util.Date;
+import java.util.Objects;
+
+public class TesseraSocio {
+	private final String idSocio;
+    private final String codiceFiscale;
+    private final Date dataAssociazione;
+    
+	public TesseraSocio(String idSocio, String codiceFiscale, Date dataAssociazione) {
+		super();
+		this.idSocio = idSocio;
+		this.codiceFiscale = codiceFiscale;
+		this.dataAssociazione = dataAssociazione;
+	}
+
+	public String getIdSocio() {
+		return idSocio;
+	}
+
+	public String getCodiceFiscale() {
+		return codiceFiscale;
+	}
+
+	public Date getDataAssociazione() {
+		return dataAssociazione;
+	}
+    
+	@Override
+    public String toString() {
+        return new StringBuilder()
+            .append("(").append(idSocio).append(") ")
+			.append(codiceFiscale).append(" ")
+			.append(dataAssociazione).toString();
+    }
+
+    @Override
+    public boolean equals(final Object other) {
+        return (other instanceof TesseraSocio)
+                && ((TesseraSocio) other).getIdSocio() == this.getIdSocio()
+                && ((TesseraSocio) other).getCodiceFiscale().equals(this.getCodiceFiscale())
+                && ((TesseraSocio) other).getDataAssociazione().equals(this.getDataAssociazione());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(this.idSocio, this.codiceFiscale, this.dataAssociazione);
+    }
+    
+    
+}

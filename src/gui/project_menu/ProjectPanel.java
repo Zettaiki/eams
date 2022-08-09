8ebcaf796a316d409b9dade4110f9c37d7cd85d7
Pablo Vargas <85257320+Zettaiki@users.noreply.github.com>
2022-08-09 23:49:53 +0200

    Update ProjectPanel.java

--- src/gui/project_menu/ProjectPanel.java
+++ src/gui/project_menu/ProjectPanel.java
@@ -1,9 +1,7 @@
 package gui.project_menu;
 
 import javax.swing.*;
-
 import utils.JComponentLoader;
-
 import java.awt.*;
 
 public class ProjectPanel extends JPanel {
@@ -14,63 +12,63 @@ public class ProjectPanel extends JPanel {
         this.c = new GridBagConstraints();
         this.setLayout(new GridBagLayout());
         
-        // First column
-        
+        // First column 
         c.gridx = 0;
         c.gridy = 0;
-        var a0 = new JLabel("Opzioni:");
+        c.fill = GridBagConstraints.VERTICAL;
+        var a0 = new JPanel();
+        a0.setBorder(BorderFactory.createTitledBorder("Opzioni:"));
+        a0.setLayout(new GridLayout(0,1));
+        {
+        	var registerButton = new JButton("Registrare progetto");
+        	registerButton.setLayout(null);
+        	registerButton.addActionListener(e -> {
+            	JFrame parentFrame = (JFrame) SwingUtilities.getWindowAncestor(this);
+    	        JComponentLoader.load(parentFrame, new InsertProjectPanel());
+            });
+        	a0.add(registerButton);
+            
+            var donatorlistButton = new JButton("Lista donatori progetto");
+            donatorlistButton.setLayout(null);
+            donatorlistButton.addActionListener(e -> {
+            	// TODO
+            });
+            a0.add(donatorlistButton);
+            
+            var filterActiveButton = new JButton("Filtra progetti attivi");
+            filterActiveButton.setLayout(null);
+            filterActiveButton.addActionListener(e -> {
+            	// TODO
+            });
+            a0.add(filterActiveButton);
+        }
         this.add(a0, c);
         
-        c.gridx = 0;
-        c.gridy = 1;
-        var a1 = new JButton("Aggiungere progetto");
-        a1.setLayout(null);
-        a1.addActionListener(e -> {
-        	JFrame parentFrame = (JFrame) SwingUtilities.getWindowAncestor(this);
-	        JComponentLoader.load(parentFrame, new InsertProjectPanel());
-        });
-        this.add(a1, c);
-        
-        c.gridx = 0;
-        c.gridy = 2;
-        var a2 = new JButton("Modificare stato progetto");
-        a2.setLayout(null);
-        a2.addActionListener(e -> {
-        	// TODO
-        });
-        this.add(a2, c);
-        
-        c.gridx = 0;
-        c.gridy = 3;
-        var a3 = new JButton("Filtra progetti attivi");
-        a3.setLayout(null);
-        a3.addActionListener(e -> {
-        	// TODO
-        });
-        this.add(a3, c);
-        
-        c.gridx = 0;
-        c.gridy = 4;
-        var a4 = new JButton("Cercare progetti");
-        a4.setLayout(null);
-        a4.addActionListener(e -> {
-        	// TODO
-        });
-        this.add(a4, c);
-        
         // Second column
-        
         c.gridx = 1;
         c.gridy = 0;
-        var b0 = new JLabel("Progetti registrati:");
-        this.add(b0, c);
-        
-        c.gridx = 1;
-        c.gridy = 1;
-        c.gridheight = 5;
-        var textArea = new JTextArea(10, 40);
-        var b1 = new JScrollPane(textArea);
-        textArea.setEditable(false);
-        this.add(b1, c);
+        c.gridwidth = 4;
+        c.fill = GridBagConstraints.VERTICAL;
+        var a1 = new JPanel();
+        a1.setBorder(BorderFactory.createTitledBorder("Progetti registrati:"));
+        a1.setLayout(new GridLayout(0,1));
+        {
+        	String[] columnNames = {"ID",
+                    "Obbiettivo",
+                    "Data inizio",
+                    "Durata mesi"};
+        	
+        	Object[][] data = {
+        		    {"-", "-", "-", "-"}
+        		};
+        	
+        	var projectTable = new JTable(data, columnNames);
+			projectTable.setEnabled(false);
+        	projectTable.getTableHeader().setReorderingAllowed(false);
+			projectTable.getTableHeader().setEnabled(false);
+        	JScrollPane projectListPanel = new JScrollPane(projectTable);
+            a1.add(projectListPanel);
+        }
+        this.add(a1, c);
     }
 }

7cccb3ea8aea38a33be6a6f98ebece4f6ea5bec7
Pablo Vargas <85257320+Zettaiki@users.noreply.github.com>
2022-08-10 01:13:05 +0200

    Create ProjectDonatorsPanel.java

---
+++ src/gui/project_menu/ProjectDonatorsPanel.java
@@ -0,0 +1,10 @@
+package gui.project_menu;
+
+import javax.swing.JPanel;
+
+public class ProjectDonatorsPanel extends JPanel {
+
+	private static final long serialVersionUID = 6590467329734162171L;
+
+	
+}

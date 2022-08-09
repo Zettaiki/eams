f2c29997e3f139dcf378d13fd0cf759e1ff3b6b6
Pablo Vargas <85257320+Zettaiki@users.noreply.github.com>
2022-08-09 23:49:41 +0200

    Update DonationPanel.java

--- src/gui/project_menu/DonationPanel.java
+++ src/gui/project_menu/DonationPanel.java
@@ -5,61 +5,71 @@ import java.awt.*;
 import utils.JComponentLoader;
 
 public class DonationPanel extends JPanel {
-    private static final long serialVersionUID = 8475751505006519027L;
+	private static final long serialVersionUID = 7637300465466931816L;
     public final GridBagConstraints c;
-    
-    public DonationPanel() {
+
+	public DonationPanel() {
         this.c = new GridBagConstraints();
-        this.setLayout(new GridBagLayout());
-        
-        // First column
-        
+		this.setLayout(new GridBagLayout());
+		
+		// First column 
         c.gridx = 0;
         c.gridy = 0;
-        var a0 = new JLabel("Opzioni:");
-        this.add(a0, c);
-        
-        c.gridx = 0;
-        c.gridy = 1;
-        var a1 = new JButton("Registrare donazione");
-        a1.setLayout(null);
-        a1.addActionListener(e -> {
-        	JFrame parentFrame = (JFrame) SwingUtilities.getWindowAncestor(this);
-	        JComponentLoader.load(parentFrame, new InsertDonationPanel());
-        });
-        this.add(a1, c);
-        
-        c.gridx = 0;
-        c.gridy = 2;
-        var a2 = new JButton("Rapporto donazione/progetto");
-        a2.setLayout(null);
-        a2.addActionListener(e -> {
-        	// TODO
-        });
-        this.add(a2, c);
-        
-        c.gridx = 0;
-        c.gridy = 3;
-        var a3 = new JButton("Donatore maggior quantita");
-        a3.setLayout(null);
-        a3.addActionListener(e -> {
-        	// TODO
-        });
-        this.add(a3, c);
-        
-        // Second column
-        
-        c.gridx = 1;
+        c.fill = GridBagConstraints.VERTICAL;
+		var a0 = new JPanel();
+		a0.setBorder(BorderFactory.createTitledBorder("Opzioni:"));
+		a0.setLayout(new GridLayout(0,1));
+		{
+			var registerButton = new JButton("Registrare donazione");
+			registerButton.setLayout(null);
+			registerButton.addActionListener(e -> {
+				JFrame parentFrame = (JFrame) SwingUtilities.getWindowAncestor(this);
+		        JComponentLoader.load(parentFrame, new InsertDonationPanel());
+		    });
+			a0.add(registerButton);
+		    
+		    var reportButton = new JButton("Rapporto donazione/progetto");
+		    reportButton.setLayout(null);
+		    reportButton.addActionListener(e -> {
+		    	// TODO
+		    });
+		    a0.add(reportButton);
+		    
+		    var maxDonatorButton = new JButton("Donatore maggior quantita");
+		    maxDonatorButton.setLayout(null);
+		    maxDonatorButton.addActionListener(e -> {
+		    	// TODO
+		    });
+		    a0.add(maxDonatorButton);
+		}
+		this.add(a0, c);
+		
+		// Second column
+		c.gridx = 1;
         c.gridy = 0;
-        var b0 = new JLabel("Registro donazioni:");
-        this.add(b0, c);
-        
-        c.gridx = 1;
-        c.gridy = 1;
-        c.gridheight = 5;
-        var textArea = new JTextArea(10, 40);
-        var b1 = new JScrollPane(textArea);
-        textArea.setEditable(false);
-        this.add(b1, c);
-    }
+        c.gridwidth = 4;
+        c.fill = GridBagConstraints.VERTICAL;
+		var a1 = new JPanel();
+		a1.setBorder(BorderFactory.createTitledBorder("Donazioni registrate:"));
+		a1.setLayout(new GridLayout(0,1));
+		{
+			String[] columnNames = {"ID",
+		            "Importo",
+		            "Codice fiscale",
+		            "Data donazione",
+		            "ID progetto"};
+			
+			Object[][] data = {
+				    {"-", "-", "-", "-", "-"},
+				};
+			
+			var donationTable = new JTable(data, columnNames);
+			donationTable.setEnabled(false);
+			donationTable.getTableHeader().setReorderingAllowed(false);
+			donationTable.getTableHeader().setEnabled(false);
+			JScrollPane projectListPanel = new JScrollPane(donationTable);
+		    a1.add(projectListPanel);
+		}
+		this.add(a1, c);
+	}
 }

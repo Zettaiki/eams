315d9d9a38a00627a1379f8f9e85abb67ee60ab0
Pablo Vargas <85257320+Zettaiki@users.noreply.github.com>
2022-08-12 19:04:16 +0200

    Create GarbageDeliverPanel.java

---
+++ src/gui/agencies_menu/GarbageDeliverPanel.java
@@ -0,0 +1,20 @@
+package gui.agencies_menu;
+
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+
+import javax.swing.JPanel;
+
+public class GarbageDeliverPanel extends JPanel {
+	private static final long serialVersionUID = 3472354135267666128L;
+	public final GridBagConstraints c;
+
+	public GarbageDeliverPanel() {
+        this.c = new GridBagConstraints();
+		this.setLayout(new GridBagLayout());
+		
+		// TODO
+	}
+	
+	
+}

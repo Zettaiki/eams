ac39162599e4be98529c4d3643ac71c1b2e693ce
Sara Romeo <sara.romeo3@studio.unibo.it>
2022-08-13 17:11:36 +0200

    Implement rank newsletter query

---
+++ src/db/query/NewsletterQuery.java
@@ -0,0 +1,43 @@
+package db.query;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+public class NewsletterQuery {
+	
+	private final Connection connection;
+
+	public NewsletterQuery(final Connection connection) {
+        this.connection = Objects.requireNonNull(connection);
+    }
+	
+	public List<String> rank() {
+		List<String> queryResultTable = new ArrayList<>();
+		
+		try (final Statement statement = this.connection.createStatement()) {
+            final ResultSet resultSet = statement.executeQuery("SELECT i.idNewsletter, " +
+            		"COUNT(*) as iscritti " +
+            		"FROM iscrizione i " +
+            		"GROUP BY i.idNewsletter " +
+            		"ORDER BY iscritti DESC");
+            try {
+    			while (resultSet.next()) {
+    				final Integer idNewsletter = resultSet.getInt("idNewsletter");
+    				final Integer iscritti = resultSet.getInt("iscritti");
+    				
+    				queryResultTable.add(new StringBuilder().append(idNewsletter).append(" ")
+    						.append(iscritti).toString());
+    			}
+    		} catch (final SQLException e) {}
+            System.out.println(queryResultTable);
+            return queryResultTable;
+        } catch (final SQLException e) {
+            throw new IllegalStateException(e);
+        }
+	}
+}

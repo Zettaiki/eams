ac7617b98135e86759ab5b0a44020ea88a50078b
Pablo Vargas <psebastian2001@hotmail.com>
2022-08-26 19:52:44 +0200

    Update DonationPanel.java

--- src/gui/project_menu/DonationPanel.java
+++ src/gui/project_menu/DonationPanel.java
@@ -16,37 +16,21 @@ public class DonationPanel extends JPanel {
 	public DonationPanel() {
 		this.setLayout(new BorderLayout());
 		
-		// First column 
+		// First panel
 		
 		var a0 = new JPanel();
 		a0.setBorder(BorderFactory.createTitledBorder("Opzioni:"));
 		a0.setLayout(new GridLayout(1,0));
-		{
+		
 			var registerButton = new JButton("Registrare donazione");
-			registerButton.addActionListener(e -> {
-				JFrame parentFrame = (JFrame) SwingUtilities.getWindowAncestor(this);
-		        JComponentLoader.load(parentFrame, new InsertDonationPanel());
-		    });
 			a0.add(registerButton);
 		    
 		    var reportButton = new JButton("Rapporto donazione/progetto");
-		    reportButton.addActionListener(e -> {
-		    	// TODO
-		    });
 		    a0.add(reportButton);
 		    
 		    var maxDonatorButton = new JButton("Donatore maggior quantita");
-		    maxDonatorButton.addActionListener(e -> {
-		    	ProjectDonatorQuery projectDonatorQuery = new ProjectDonatorQuery(ConnectionProvider.getMySQLConnection());
-        		JOptionPane.showMessageDialog(getParent(), "Donatore con maggiore quantita:\n" +
-		    	"Codice fiscale: " + projectDonatorQuery.bestDonator().get().get(0)[0].toString() +
-		    	"\nNome: " + projectDonatorQuery.bestDonator().get().get(0)[1].toString() +
-		    	"\nCognome: " + projectDonatorQuery.bestDonator().get().get(0)[2].toString() +
-		    	"\nValue: " + projectDonatorQuery.bestDonator().get().get(0)[3].toString(), 
-        		"Informazione", JOptionPane.INFORMATION_MESSAGE);
-		    });
 		    a0.add(maxDonatorButton);
-		}
+		
 		this.add(a0, BorderLayout.PAGE_START);
 		
 		// Second column
@@ -54,14 +38,35 @@ public class DonationPanel extends JPanel {
 		var a1 = new JPanel();
 		a1.setBorder(BorderFactory.createTitledBorder("Donazioni registrate:"));
 		a1.setLayout(new GridLayout(0,1));
-		{
+		
 			var donationTable = new JTable(TableExtractorUtils.donationTable());
 			donationTable.setEnabled(false);
 			donationTable.getTableHeader().setReorderingAllowed(false);
 			donationTable.getTableHeader().setEnabled(false);
 			JScrollPane donationListPanel = new JScrollPane(donationTable);
 		    a1.add(donationListPanel);
-		}
+		
 		this.add(a1, BorderLayout.CENTER);
+		
+		// Action listeners
+		
+		registerButton.addActionListener(e -> {
+			JFrame parentFrame = (JFrame) SwingUtilities.getWindowAncestor(this);
+	        JComponentLoader.load(parentFrame, new InsertDonationPanel());
+	    });
+		
+		reportButton.addActionListener(e -> {
+	    	// TODO
+	    });
+		
+		maxDonatorButton.addActionListener(e -> {
+	    	ProjectDonatorQuery projectDonatorQuery = new ProjectDonatorQuery(ConnectionProvider.getMySQLConnection());
+    		JOptionPane.showMessageDialog(getParent(), "Donatore con maggiore quantita:\n" +
+	    	"Codice fiscale: " + projectDonatorQuery.bestDonator().get().get(0)[0].toString() +
+	    	"\nNome: " + projectDonatorQuery.bestDonator().get().get(0)[1].toString() +
+	    	"\nCognome: " + projectDonatorQuery.bestDonator().get().get(0)[2].toString() +
+	    	"\nValue: " + projectDonatorQuery.bestDonator().get().get(0)[3].toString(), 
+    		"Informazione", JOptionPane.INFORMATION_MESSAGE);
+	    });
 	}
 }

7133dcc8303498f99ebe2f97527456e4bf6f6ddb
Pablo Vargas <psebastian2001@hotmail.com>
2022-08-26 13:23:14 +0200

    Update ServiceSearchPanel.java

--- src/gui/events_menu/ServiceSearchPanel.java
+++ src/gui/events_menu/ServiceSearchPanel.java
@@ -1,7 +1,6 @@
 package gui.events_menu;
 
-import java.awt.GridBagConstraints;
-import java.awt.GridBagLayout;
+import java.awt.BorderLayout;
 import java.awt.GridLayout;
 
 import javax.swing.BorderFactory;
@@ -19,74 +18,64 @@ import utils.TableExtractorUtils;
 
 public class ServiceSearchPanel extends JPanel {
 	private static final long serialVersionUID = 543534484180509595L;
-
-	 public final GridBagConstraints c;
 		
 		public ServiceSearchPanel() {
-			this.c = new GridBagConstraints();
-			this.setLayout(new GridBagLayout());
+			this.setLayout(new BorderLayout());
 			
-			// First column 
-	        c.gridx = 0;
-	        c.gridy = 0;
-	        c.fill = GridBagConstraints.HORIZONTAL;
+			// First panel
+			
 			var a0 = new JPanel();
 			a0.setBorder(BorderFactory.createTitledBorder("Opzioni di ricerca servizi:"));
 			a0.setLayout(new GridLayout(1,2));
-			{
+			
 				String[] searchOptions = {"Ricerca per ID evento", "Ricerca per tipo servizio"};
 				
 				var searchOptionsBox = new JComboBox<String>(searchOptions);
 				searchOptionsBox.setBorder(BorderFactory.createTitledBorder("Tipo di ricerca:"));
-				searchOptionsBox.addActionListener(e -> {
-					// TODO
-			    });
 				a0.add(searchOptionsBox);
 			    
 			    var searchField = new JTextField(16);
 			    searchField.setBorder(BorderFactory.createTitledBorder("Dato da cercare:"));
-			    searchField.addActionListener(e -> {
-			    	// TODO
-			    });
 			    a0.add(searchField);
 			    
 			    var searchButton = new JButton("Cercare servizio");
-			    searchButton.addActionListener(e -> {
-					// TODO
-			    });
 				a0.add(searchButton);
 			    
-			}
-			this.add(a0, c);
+			this.add(a0, BorderLayout.PAGE_START);
 			
-			// Second column
-			c.gridx = 0;
-	        c.gridy = 1;
-	        c.gridheight = 4;
-	        c.fill = GridBagConstraints.HORIZONTAL;
+			// Mid panel
+			
 			var a1 = new JPanel();
 			a1.setBorder(BorderFactory.createTitledBorder("Risultati servizi cercati:"));
 			a1.setLayout(new GridLayout(0,1));
-			{
-				var donationTable = new JTable(TableExtractorUtils.serviceTable());
-				donationTable.setEnabled(false);
-				donationTable.getTableHeader().setReorderingAllowed(false);
-				donationTable.getTableHeader().setEnabled(false);
-				JScrollPane projectListPanel = new JScrollPane(donationTable);
-			    a1.add(projectListPanel);
-			}
-			this.add(a1, c);
 			
-			// Third column
-			c.gridx = 0;
-	        c.gridy = 6;
-	        c.gridheight = 1;
-	        c.fill = GridBagConstraints.HORIZONTAL;
+				var serviceTable = new JTable(TableExtractorUtils.serviceTable());
+				serviceTable.setEnabled(false);
+				serviceTable.getTableHeader().setReorderingAllowed(false);
+				serviceTable.getTableHeader().setEnabled(false);
+				JScrollPane projectListPanel = new JScrollPane(serviceTable);
+			    a1.add(projectListPanel);
+			
+			this.add(a1, BorderLayout.CENTER);
+			
+			// Bottom panel
+			
 			var a2 = new JButton("Iscrivere volontario ad un servizio");
+			this.add(a2, BorderLayout.PAGE_END);
+			
+			// Action listeners
+			
+			searchButton.addActionListener(e -> {
+				if(searchOptionsBox.getSelectedIndex() == 0) {
+					serviceTable.setModel(TableExtractorUtils.serviceSearchByID(searchField.getText()));
+				} else if(searchOptionsBox.getSelectedIndex() == 1) {
+					serviceTable.setModel(TableExtractorUtils.serviceSearchByType(searchField.getText()));
+				}
+		    });
+			
 			a2.addActionListener(e -> {
 				JFrame parentFrame = (JFrame) SwingUtilities.getWindowAncestor(this);
 		        JComponentLoader.load(parentFrame, new VolunteerRegisterServicePanel());
 		    });
-			this.add(a2, c);
 	    }
 }

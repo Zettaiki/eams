49497698c94a77478ebce74781478cb7139bf116
Pablo Vargas <psebastian2001@hotmail.com>
2022-08-30 16:21:43 +0200

    Create InsertBuyPanel.java

---
+++ src/gui/commerce_menu/InsertBuyPanel.java
@@ -0,0 +1,12 @@
+package gui.commerce_menu;
+
+import javax.swing.JPanel;
+
+public class InsertBuyPanel extends JPanel {
+	private static final long serialVersionUID = 3807718932905995172L;
+	
+	public InsertBuyPanel() {
+		
+	}
+	
+}

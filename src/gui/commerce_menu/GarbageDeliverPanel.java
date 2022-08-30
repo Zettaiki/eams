00f7d5a3ef61801e3088a5b3b95420bece8e9742
Pablo Vargas <psebastian2001@hotmail.com>
2022-08-30 16:21:46 +0200

    Update GarbageDeliverPanel.java

--- src/gui/commerce_menu/GarbageDeliverPanel.java
+++ src/gui/commerce_menu/GarbageDeliverPanel.java
@@ -1,20 +1,19 @@
 package gui.commerce_menu;
 
-import java.awt.GridBagConstraints;
-import java.awt.GridBagLayout;
+import java.awt.BorderLayout;
 
 import javax.swing.JPanel;
 
 public class GarbageDeliverPanel extends JPanel {
 	private static final long serialVersionUID = 3472354135267666128L;
-	public final GridBagConstraints c;
 
 	public GarbageDeliverPanel() {
-        this.c = new GridBagConstraints();
-		this.setLayout(new GridBagLayout());
+		this.setLayout(new BorderLayout());
+		
+		
+		
 		
 		// TODO
 	}
-	
 	
 }

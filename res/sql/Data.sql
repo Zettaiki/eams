2fa78f4deda62df83068f436fd31ab952cfe4542
Pablo Vargas <85257320+Zettaiki@users.noreply.github.com>
2022-12-27 21:29:45 +0100

    Update Data.sql

--- res/sql/Data.sql
+++ res/sql/Data.sql
@@ -1,10 +1,10 @@
 CREATE DATABASE  IF NOT EXISTS `eams` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
 USE `eams`;
--- MySQL dump 10.13  Distrib 8.0.30, for Win64 (x86_64)
+-- MySQL dump 10.13  Distrib 8.0.29, for Win64 (x86_64)
 --
 -- Host: localhost    Database: eams
 -- ------------------------------------------------------
--- Server version	8.0.30
+-- Server version	8.0.29
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -143,7 +143,7 @@ CREATE TABLE `donazione` (
   `importo` decimal(11,2) NOT NULL,
   `codiceFiscale` char(16) NOT NULL,
   `dataDonazione` date NOT NULL,
-  `tipo` VARCHAR(45) NOT NULL,
+  `tipo` varchar(45) NOT NULL,
   `idProgetto` int DEFAULT NULL,
   PRIMARY KEY (`idDonazione`),
   KEY `fk_donazione_progetto1_idx` (`idProgetto`),
@@ -587,7 +587,7 @@ DROP TABLE IF EXISTS `tesserasocio`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `tesserasocio` (
-  `idSocio` INT NOT NULL AUTO_INCREMENT,
+  `idSocio` int NOT NULL AUTO_INCREMENT,
   `codiceFiscale` char(16) NOT NULL,
   `dataAssociazione` date NOT NULL,
   PRIMARY KEY (`idSocio`),
@@ -719,4 +719,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2022-08-30 16:36:46
+-- Dump completed on 2022-12-27 21:29:21
